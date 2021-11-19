d1d07bd93d6a9b0f9493167b46f64bed786c26b6
Anish <87910771+AnishLohiya@users.noreply.github.com>
2021-11-19 15:06:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // variables
-
 const cartBtn = document.querySelector(".cart-btn");
 const closeCartBtn = document.querySelector(".close-cart");
 const clearCartBtn = document.querySelector(".clear-cart");
